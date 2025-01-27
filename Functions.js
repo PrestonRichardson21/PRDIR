b84ecfc7ffdd61c7d806ffbe5285481f2e7d7b22
Preston Richardson <167050211+PrestonRichardson21@users.noreply.github.com>
2025-01-27 16:15:11 -0600

    update

--- Functions.js
+++ Functions.js
@@ -1,21 +1,19 @@
 const fs = require('fs');
-const {parseString, Builder} = require('xml2js');
+const { parseString, Builder } = require('xml2js');
 const XLSX = require('xlsx');
 
-let salesInv = [];
-let agvanceData = [];
-let duplicates = [];
-   
-
+// Function to remove the first digit from an invoice number
+function removeFirstDigit(invoiceNumber) {
+    return invoiceNumber.substring(1);
+}
 
 let sdetLoad = (salesFilePath) => {
-        //Querying the XML input file
-    
-        const xml = fs.readFileSync(salesFilePath).toString();
+    // Querying the XML input file
+    const xml = fs.readFileSync(salesFilePath).toString();
+    const salesInv = [];
 
     // const xml = fs.readFileSync(salesFilePath).toString();
-    parseString(xml, function(err, SDET) {
-        
+    parseString(xml, function (err, SDET) {
         if (err) {
             console.error("Error parsing XML:", err);
             return;
@@ -35,69 +33,120 @@ let sdetLoad = (salesFilePath) => {
         } else {
             console.error("Expected XML structure not found.");
         }
-        return salesInv;
-        
     });
-    return xml;
+
+    return {xml, salesInv};
 };
 
 
 let excelLoad = (excelFilePath) => {
-//Loading the Agvance Data file 
+    const agvanceData = [];
 
+    //Loading the Agvance Data file 
     const workbook = XLSX.readFile(excelFilePath);
-// const workbook = XLSX.readFile(excelFilePath);
-const sheetNames = workbook.SheetNames;
-const sheet = workbook.Sheets[sheetNames[0]]; // Assuming you want the first sheet
-const data = XLSX.utils.sheet_to_json(sheet);
-console.log(data.length);
-// Turns an array of objects into an array of strings
-for (let i = 0; i < data.length; i++){
-    let inv = {};
-    let tickTotal = parseFloat(data[i].TicketTotal).toFixed(2);
-    // agvanceData.push(data[i].ControlNumber + tickTotal);
-    inv["ControlNumber"] = { value: data[i].ControlNumber};
-    inv["TicketTotal"] = {value: tickTotal};
-    agvanceData.push(inv);
-};
+    // const workbook = XLSX.readFile(excelFilePath);
+    const sheetNames = workbook.SheetNames;
+    const sheet = workbook.Sheets[sheetNames[0]]; // Assuming you want the first sheet
+    const data = XLSX.utils.sheet_to_json(sheet);
+  
+    // Turns an array of objects into an array of strings
+    for (let i = 0; i < data.length; i++) {
+        const inv = {};
+        const tickTotal = parseFloat(data[i].TicketTotal).toFixed(2);
+        // agvanceData.push(data[i].ControlNumber + tickTotal);
+        inv["ControlNumber"] = { value: data[i].ControlNumber };
+        inv["TicketTotal"] = { value: tickTotal };
+        agvanceData.push(inv);
+    };
+
+    return agvanceData;
+
 };
 
-let compare = (xml) =>{
-//Creates a list of duplicate entries that has the same invoice number and ticket total
+let compare = (xml, salesInv, agvanceData) => {
+    //Creates a list of duplicate entries that has the same invoice number and ticket total
+    const duplicates = [];
+    const duplicateInvoiceNumbers = [];
+    let invoiceCount = {};
+    
+    for (let i = 0; i < salesInv.length; i++) {
+        let invoiceNumber = salesInv[i].InvoiceNumber.value;
+        invoiceCount[invoiceNumber] = (invoiceCount[invoiceNumber] || 0) + 1;
 
-for(let i = 0; i < salesInv.length; i++){
-    if (agvanceData.some(inv => inv.ControlNumber.value === salesInv[i].InvoiceNumber.value )&& agvanceData.some(inv => inv.TicketTotal.value === salesInv[i].InvoiceTotal.value)){
+        if (agvanceData.some(inv => inv.ControlNumber.value === salesInv[i].InvoiceNumber.value) && agvanceData.some(inv => inv.TicketTotal.value === salesInv[i].InvoiceTotal.value)) {
             duplicates.push(salesInv[i].InvoiceNumber.value);
-        } else {
-        //  console.log("No invoices found or the first invoice does not have an InvoiceNumber.");
+            // console.log(`${salesInv[i].InvoiceNumber.value} is a duplicate`);
+        } else if (agvanceData.some(inv => inv.ControlNumber.value === salesInv[i].InvoiceNumber.value) && !agvanceData.some(inv => inv.TicketTotal.value === salesInv[i].InvoiceTotal.value)) {
+            duplicateInvoiceNumbers.push(salesInv[i].InvoiceNumber.value);
+            
+            //  console.log(`${salesInv[i].InvoiceNumber.value} has a different Ticket Total`);
+        } else if (invoiceCount[invoiceNumber] > 1 ) {
+            duplicateInvoiceNumbers.push(invoiceNumber);
+            console.log(`${invoiceNumber} has a different Ticket Total`);
+
         }
+      
     };
 
     // Remove all duplicates from the original string after processing them
-    for(let i = 0; i < duplicates.length; i++){
+    for (let i = 0; i < duplicates.length; i++) {
         const regex = new RegExp(`<InvoiceNumber>${duplicates[i]}[\\s\\S]*?<\\/Invoice>`, 'gi');
-        if(xml.match())
-        xml = xml.replace(regex, '');
+        if (xml.match())
+            xml = xml.replace(regex, '');
     };
-    for(let i = 0; i < duplicates.length; i++){
+    for (let i = 0; i < duplicates.length; i++) {
 
         xml = xml.replace(/<Invoice type="CHARGE">[\s][\s][\s][\s][\s][\s][\s][\s][\s][\s][\s][\s][\s][\s][\s][\s][\s][\s][\s]/, '');
     };
-    console.log(`Duplicates before clearing: ${duplicates.length}`);
-//  Write the modified content back to the file
-fs.writeFileSync('test.xml', xml, 'utf8');
-duplicates = [];
-data = [];
-console.log(`Duplicates after clearing: ${duplicates.length}`);
+
+
+    // change the duplicate invoice number to a different invoice number
+
+    const duplicateCount = {};
+
+    for (let i = 0; i < duplicateInvoiceNumbers.length; i++) {
+        const originalInvoiceNumber = duplicateInvoiceNumbers[i];
+        firstDigitOfInvoiceNumber = Number(String(originalInvoiceNumber).charAt(0));
+
+        if (!duplicateCount[originalInvoiceNumber]) {
+            duplicateCount[originalInvoiceNumber] = 0;
+        }
+
+  
+        let prefix = 9 - duplicateCount[originalInvoiceNumber];
+  
+        //making sure the prefix is not the same as the first digit of the invoice number because then the invoice number will be the same
+        if (prefix === firstDigitOfInvoiceNumber && prefix < 9) {
+            prefix++;
+        }else{
+            prefix--;
+        }
+
+    
+
+        [originalInvoiceNumber];
+        duplicateCount[originalInvoiceNumber]++;
+
+        let changedInvoiceNumber = prefix + removeFirstDigit(originalInvoiceNumber);
+        console.log(`Changed Invoice Number: ${changedInvoiceNumber}`);
+        const regex = new RegExp(`<InvoiceNumber>${originalInvoiceNumber}[\\s\\S]*?<\\/InvoiceNumber>`, 'i');
+        xml = xml.replace(regex, `<InvoiceNumber>${changedInvoiceNumber}</InvoiceNumber>`);
+        
+    }
+
+   
+
+    //  Write the modified content back to the file
+    fs.writeFileSync('updated.xml', xml, 'utf8');
+    return { duplicates, duplicateInvoiceNumbers};
 };
 
 
 let run = (salesFilePath, excelFilePath) => {
-    console.log('Hello World');
-    const xml = sdetLoad(salesFilePath);
-    excelLoad(excelFilePath);
-    compare(xml);
-
+    const {xml, salesInv} = sdetLoad(salesFilePath);
+    const agvanceData = excelLoad(excelFilePath);
+    const {duplicates, duplicateInvoiceNumbers} = compare(xml, salesInv, agvanceData);
+    return {duplicates, duplicateInvoiceNumbers};
 };
 
-module.exports = {run };
+module.exports = { run };
--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const path = require('path');
 const app = express();
 const hostname = '127.0.0.1';
 const port = 3000;
-const { run } = require ('./Functions.js');
+let { run} = require ('./Functions.js');
+
 
 const fileStorageEngine = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -18,34 +19,71 @@ const fileStorageEngine = multer.diskStorage({
     },
 });
 
+const clearDataFiles = () => {
+    const directory = './dataFiles';
+    fs.readdir(directory, (err, files) => {
+        if (err) throw err;
+        for (const file of files) {
+            fs.unlink(path.join(directory, file), err => {
+                if (err) throw err;
+            });
+        }
+    });
+};
+
 const upload = multer({ storage: fileStorageEngine });
 const multipleUpload = upload.fields([
     { name: 'xmlFile', maxCount: 1 },
     { name: 'excelFile', maxCount: 1 }
 ]);
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 app.post('/upload', multipleUpload, (req, res) => {
-    const salesFile = req.files['xmlFile'][0];
-    const excelFile = req.files['excelFile'][0];
-
-    // Process the files as needed
-    console.log(`Sales File: ${salesFile.path}`);
-    console.log(`Excel File: ${excelFile.path}`);
+    const salesFile = req.files['xmlFile'] ? req.files['xmlFile'][0] : null;
+    const excelFile = req.files['excelFile'] ? req.files['excelFile'][0] : null;
+        // Check if either file is missing
+        if (!salesFile || !excelFile) {
+            return res.send(`
+                <link rel="stylesheet" type="text/css" href="/style.css">
+                <h1>Upload Failed</h1>
+                <p>Both XML and Excel files are required.</p>
+                <button onclick="window.location.href='/'">Go Back to Home</button>
+            `);
+        }
 
     // Call your function to process the files
-    run(salesFile.path, excelFile.path);
+    const {duplicates, duplicateInvoiceNumbers} = run(salesFile.path, excelFile.path);
+        clearDataFiles();
 
-    // Provide a link to download the test.xml file
+    // Provide a link to download the updated.xml file
     res.send(`
+        <link rel="stylesheet" type="text/css" href="/style.css">
+        <div>
+            <button onclick="window.location.href='/'">Go Back to Home</button>
+            <a href="/download/updated.xml" download>Download updated.xml</a>
+        </div>
         <h1>Files uploaded and processed successfully</h1>
-        <a href="/download/test.xml" download>Download test.xml</a>
+        <h2># of Duplicates Removed:</h2>
+        <h3> ${duplicates.length} </h3>
+        <h2># of invoice numbers changed:</h2>
+        <h3> ${duplicateInvoiceNumbers.length}</h3>
+        <h2>Removed Duplicates</h2>
+        <ul class="grid">
+            ${duplicates.map(duplicates => `<li>${duplicates}</li>`).join('')}
+        </ul>
+        <h2>Changed Invoice Numbers</h2>
+        <ul class="grid">
+            ${duplicateInvoiceNumbers.map(invoiceNumber => `<li>${invoiceNumber}</li>`).join('')}
+        </ul>
+
     `);
 });
-app.get('/download/test.xml', (req, res) => {
-    const file = path.join(__dirname, 'test.xml');
+
+app.get('/download/updated.xml', (req, res) => {
+    const file = path.join(__dirname, 'updated.xml');
     res.download(file);
 });
 
